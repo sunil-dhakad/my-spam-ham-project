c8945767b11a599b5a15f7254886967bbcd588fb
sunil-dhakd <0to1.sunil@gmail.com>
2023-09-30 03:22:08 +0530

    model trainer completed

--- spamham/components/modeltrainer.py
+++ spamham/components/modeltrainer.py
@@ -8,7 +8,18 @@ import re
 from spamham.utils.utils import remove_stopwords
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 import os,sys
- 
+ import joblib
+from spamham.utils.utils import tune_and_fit
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+import mlflow
+
+
+
+
 
 class Modeltrainerclass:
     
@@ -19,11 +30,77 @@ class Modeltrainerclass:
 
         except Exception as e:
             raise CustomException(e,sys) from e
-        
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+            
+        def inititat_model_training(self,xtrain,xtest,ytrain,ytest):
+            try:
 
-    def inititat_model_traininf(self):
 
-        
+                model_obj=  {'randomforestclassifier' : RandomForestClassifier(),
+                            'svm_classifier' : SVC(),
+                            'logistic_regression' : LogisticRegression(),
+                            'multinomial_naive_bayse' : MultinomialNB()
+                            }
+
+                param_grids = {"randomforestclassifier":{"n_estimators": [50,100,150,200,250],
+                                                            "max_depth": [1,3,5,7],
+                                                            "criterion": ["gini","entropy"]
+                                                            },
+
+                                        "svm_classifier": {'C': [0.1,1,10],
+                                                        'kernel':['linear','rbf'],
+                                                        'gamma': ['scale','auto',0.1],
+                                                        'degree': [2,3,4]},
+
+
+                                    "logistic_regression": {'C': [0.001, 0.01, 0.1, 1, 10],           
+                                                        'penalty': ['l1', 'l2'],                  
+                                                        'max_iter': [100, 200, 300],              
+                                                        'solver': ['liblinear', 'saga'],          
+                                                        'multi_class': ['ovr', 'multinomial'],    
+                                                        'class_weight': [None, 'balanced']},
+
+                        
+                                "multinomial_naive_bayse":{'alpha': [0.1, 0.5, 1.0, 1.5, 2.0]}
+
+                        }
+
+
+                result_model_r2 = tune_and_fit(model_obj=model_obj,param_grids=param_grids,xtrain=xtrain,xtest=xtest,ytrain=ytrain,ytest=ytest)
+                best_model_willbe = max(sorted(result_model_r2.values()))
+                best_model = list(model_obj.keys()).index(best_model_willbe)
+
+                obj_best_model = model_obj[best_model]
+
+                
+                os.makedirs(self.model_trainer_config.trained_models,exist_ok=True)
+                model_path =os.path.join(self.model_trainer_config.trained_models,'model_file_name')
+                joblib.dump(obj_best_model,model_path)
+                
+                r2_score = obj_best_model.predict(ytest,obj_best_model.pridect(xtest))
+                return r2_score
+            except Exception as e:
+                raise CustomException(e,sys) from e
+
+
+
+        def initiate_model_trainer(self):
+            try:
+                train_set = pd.read_csv(self.model_trainer_config.train_data)
+                test_set =pd.read_csv(self.model_trainer_config.test_data)
+
+                xtrain =train_set[:,:-1]
+                ytrain = train_set[:-1]
+                xtest = test_set[:,:-1]
+                ytest = test_set[:-1]
+
+# should i write here mlflow code?????
+
+                self.inititat_model_training(xtrain,xtest,ytrain,ytest)
+            except Exception as e:
+                raise CustomException(e,sys) from e
+
 
 
 
--- spamham/utils/utils.py
+++ spamham/utils/utils.py
@@ -4,6 +4,8 @@ from spamham.logger import logging
 import nltk
 from nltk.corpus import stopwords
 nltk.download('stopwords', quiet=True)
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score,mean_squared_error
 
 def read_yalm_function(file_path:str)->dict:
     try:
@@ -20,3 +22,36 @@ def remove_stopwords(text):
     words = text.split()
     filtered_words = [word for word in words if word.lower() not in stop_words_list]
     return ' '.join(filtered_words)
+
+
+
+
+def tune_and_fit(model_obj,param_grids,xtrn,xtest,ytrn,ytest):
+    try:
+        for ith_model in range(list(model_obj())):
+            model_obj = model_obj.values()[ith_model]
+            param_gridsrch = param_grids(model_obj.keys())[ith_model]
+
+            grid_model = GridSearchCV(model_obj,param_gridsrch,cv=4)
+            grid_model.fit(xtrn,ytrn)
+
+            model_obj.set_params(**grid_model.best_params_)
+
+            model_obj.fit(xtrn,ytrn)
+
+            y_pred = model_obj.predict(xtest)
+            r2_score = r2_score(ytest,y_pred)
+            mse = mean_squared_error(ytest,y_pred)
+            rmse = mse**0.5 
+            
+            result[list(model_obj.keys()[ith_model])] = r2_score
+            
+        return result
+
+    except Exception as e:
+        raise CustomException(e,sys) from e
+
+
+
+        
+
